00a58df31a5431f42f579142c28eddc6924b5f89
franclinwat <franclinwat@yahoo.fr>
2026-04-30 17:43:11 +0300

    chore: initialisation projet EduTrack
    - Spring Boot 3.2 + Java 17
    - Preparation entretien technique
    - Themes couverts : JPA, Security, JWT, Stream API, Angular

---
+++ src/main/java/com/edutrack/GestionScolaireSimplifieeApplication.java
@@ -0,0 +1,13 @@
+package com.edutrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionScolaireSimplifieeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionScolaireSimplifieeApplication.class, args);
+	}
+
+}
