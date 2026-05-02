04c4e1ebfa9f3bc8d170594996bfc2b580a88c84
franclinwat <francliwatc@gmail.com>
2026-05-02 23:51:34 +0300

    feat: entites Cours et Note avec relations JPA
    - Cours : @ManyToOne vers Professeur (1 prof -> N cours)
    - Cours : @OneToMany vers Note (1 cours -> N notes)
    - Note  : @ManyToOne vers Etudiant et vers Cours
    - @JoinColumn : noms explicites des cles etrangeres
    - FetchType.LAZY : chargement a la demande
    - @Builder.Default : initialisation liste notes
    - Prepare pour indexation (codeCours) et partition (semestre)

--- src/main/java/com/edutrack/model/Note.java
+++ src/main/java/com/edutrack/model/Note.java
@@ -1,5 +1,6 @@
 package com.edutrack.model;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
@@ -9,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.engine.internal.Nullability;
 
 import java.time.LocalDate;
 
